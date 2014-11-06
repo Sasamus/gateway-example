7219c4df7751996093d18aa81f1a3bfc144713ab
Sasamus <labus93@gmail.com>
2014-11-06 13:09:21 +0100

    Created Gateway

---
+++ src/ae/gateway_example/Gateway.java
@@ -0,0 +1,16 @@
+package ae.gateway_example;
+
+/**
+ * @author Albin Engström
+ * @since  2014-11-06
+ */
+public class Gateway {
+
+	/**
+	 * Constructor
+	 */
+	public Gateway() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/ae/gateway_example/GatewayUser.java
+++ src/ae/gateway_example/GatewayUser.java
@@ -1,14 +1,24 @@
 package ae.gateway_example;
 
 /**
- * A class that uses a gateway to access data external file
+ * A class that uses a gateway to access data from an external file
  * 
  * @author Albin Engström
  * @since  2014-11-06
  */
 public class GatewayUser {
 	
-	public void PrintData() {
+	/**
+	 * Constructor
+	 */
+	public GatewayUser() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * Prints data
+	 */
+	public void printData() {
 		
 		
 	}
