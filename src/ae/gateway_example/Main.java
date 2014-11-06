44375031f624477b66f468fc0202a2ef8d3c8c6e
Sasamus <labus93@gmail.com>
2014-11-06 12:06:14 +0100

    Created the Main file

---
+++ src/ae/gateway_example/Main.java
@@ -0,0 +1,22 @@
+package ae.gateway_example;
+
+/**
+ * main for a example of the gateway design pattern
+ * 
+ * @author Albin Engström
+ * @since 2014-11-06
+ *
+ */
+public class Main {
+
+	/**
+	 * the main function
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
